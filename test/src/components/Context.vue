0316432c52bc6c4bd1a2fc9ddb1b109c3e2178df
jilon <wangjinlong@xuejj.com>
2021-12-06 10:34:10 +0800

    test: 测试数据修改

--- test/src/components/Context.vue
+++ test/src/components/Context.vue
@@ -1,18 +1,17 @@
 <template>
 	<section>
-		<p>{{ num }}</p>
+		<p @click="add">{{ num }}</p>
 		<p v-text="context.state"></p>
 	</section>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, reactive, onMounted } from 'vue';
-
+import { defineComponent, ref, reactive, onMounted, onUpdated } from 'vue';
 
 export default defineComponent({
 	name: 'Context',
 	setup() {
-		type IContext = {
+		interface IContext {
 			state: string
 			formList: string[]
 		}
@@ -23,19 +22,37 @@ export default defineComponent({
 			formList: []
 		})
 
+		function getTestData() {
+			return new Promise((resolve, reject) => {
+				setTimeout(() => {
+					resolve('this is test content')
+				}, 1000 * 8)
+			})
+		}
+
+		function add() {
+			num.value = num.value + 1;
+		}
+
 		onMounted(() => {
 			console.log(context.state)
+			getTestData().then(res => {
+				console.log(res)
+			});
+
 			setTimeout(() => {
 				context.state = 'context-test';
-				num = 'num' // Type 'string' is not assignable to type 'Ref<number>'
+				// num = 'num' // Type 'string' is not assignable to type 'Ref<number>'
 				context.formList.push('formList first data')
-				context.formList.push(new Date()) // Argument of type 'Date' is not assignable to parameter of type 'string'
-				context.formList.push(1) // Argument of type 'number' is not assignable to parameter of type 'string'
+				// context.formList.push(new Date()) // Argument of type 'Date' is not assignable to parameter of type 'string'
+				// context.formList.push(1) // Argument of type 'number' is not assignable to parameter of type 'string'
 			}, 1000 * 3)
 		})
+
 		return {
 			num,
-			context
+			context,
+			add
 		}
 	}
 })
